9f4ca8c803c6362d70fdfa2e9616405878229760
daromi94 <davromero07@gmail.com>
2024-11-24 09:55:44 -0500

    refactor(infra-craft): :truck: use class a ip address ranges for private networks

--- src/infra-craft/environments/dev/main.tf
+++ src/infra-craft/environments/dev/main.tf
@@ -1,6 +1,7 @@
 module "network" {
-  source                     = "../../modules/network"
-  vpc_cidr                   = "172.16.0.0/16"
-  private_subnet_cidrs       = ["172.16.0.0/20", "172.16.16.0/20"]
-  private_availability_zones = ["us-east-1a", "us-east-1b"]
+  source = "../../modules/network"
+
+  vpc_cidr                   = "10.0.0.0/16"
+  private_subnet_cidrs       = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
